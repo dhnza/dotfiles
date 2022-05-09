8cbbd641f1bf39489aa4ad9477c8ccd443898d44
David Neill-Asanza <davidhneill@gmail.com>
2022-05-09 22:19:07 +0200

    Added delta to cargo packages.

--- .dotfiles-install.sh
+++ .dotfiles-install.sh
@@ -73,7 +73,7 @@ fi
 
 # rust packages
 chk_command "cargo"
-cargo install fd-find ripgrep bat
+cargo install fd-find ripgrep bat git-delta
 
 # vim plugins
 vim +PlugUpdate +'w! /tmp/vim-plug.log' +qall <<< "\n" > /dev/null
